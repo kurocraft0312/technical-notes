bed063f734833cd8a66ab79b8e9be91cae7c828c
kurocraft0312 <kurocraft0312@gmail.com>
2019-08-07 21:14:05 +0900

    ファイルを追嫁しました。

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'technical-notes',
+    description: '調べたことをひたすらメモするだけの備忘録です。',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'About', link: '/about.html' },
+            { text: 'Contact', link: '/contact.html' }
+        ]
+    }
+}
