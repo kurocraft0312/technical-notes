a9afab0f78b638a79796d28965518374a8b6f25c
kurocraft0312 <kurocraft0312@gmail.com>
2019-08-08 18:16:26 +0900

    カテゴリ一覧ページを制作中。

---
+++ docs/.vuepress/theme/layouts/Categories.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="page">
+        <div class="cat_contents">
+            <h2>カテゴリ一覧</h2>
+            <div>
+                
+            </div>
+        </div>
+    </div>
+</template>
